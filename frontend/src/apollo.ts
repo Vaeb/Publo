844bb786c83cc36b4e1602da0e89b4988493816e
Adam Johnson <vaeb@hey.com>
2021-04-26 15:55:51 +0100

    webServer

--- frontend/src/apollo.ts
+++ frontend/src/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const webServer = false;
+const webServer = true;
 
 const client = new ApolloClient({
     uri: webServer ? 'http://vaeb.io:4000/graphql' : 'http://localhost:4000/graphql',
